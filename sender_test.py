0bd07cc3d85ae89f54af7d37b16434991f50b519
leejunsung99 <htjun99@naver.com>
2022-12-13 15:36:08 +0900

    fix sender_test.py

--- sender_test.py
+++ sender_test.py
@@ -11,12 +11,9 @@ import base64
 def send_msg(interface, network, performance,Uid ,msg):
     sock = RawSocket(interface, 0xEEFA)
     sock.Uid = Uid
-    global Stime
     if msg=='Discover':
-        sock.send(to_str(len(msg)))
+        sock.send(to_str(len(msg)).ljust(64))
         sock.send(msg)
-        Stime = time.time()
-        sock.send(to_str(Stime))
         print('\n'+interface+' send: '+msg)
         packet = sock.recv()
         print("Discover-- "+interface+": "+to_str(packet.src))    
@@ -25,27 +22,20 @@ def send_msg(interface, network, performance,Uid ,msg):
         print('\n'+interface+' send: '+msg)
     else:
         Dmsg = Divide_msg(interface, network, performance, msg)
-        sock.send(to_str(len(Dmsg)))
-        Stime = time.time()
+        sock.send(to_str(len(Dmsg)).ljust(64))
         sock.sendall(Dmsg)
-        sock.send(to_str(Stime))
-        print('send time: ',Stime)
         print('\n'+interface+' send: '+Dmsg)
 
 def send_img(interface, network, performance,Uid ,img):
-    global Stime
     sock = RawSocket(interface, 0xEEFA)
     sock.Uid = Uid 
     Bimg = base64.b64encode(cv2.imencode('.jpg',img)[1]).decode('utf-8')
     Dimg = Divide_msg(interface, network, performance, Bimg)
-
-    sock.send(to_str(len(Dimg)))
-    Stime = time.time()
+    sock.send(to_str(len(Dimg)).ljust(64))
     sock.sendall(to_bytes(Dimg))
-    sock.send(to_str(Stime))
-    print('send time: ',Stime)
     print('\n'+interface+' send: ',end='')
     print(to_bytes(Dimg))
+    print('done')
 
 def Divide_msg(interface, network, performance, msg):
     for i in range(len(network)):
@@ -83,6 +73,9 @@ def Unique_id(network):
 def main():
     network = find_network()
     performance = net_pf(network)
+    #test
+    #network = ['eth0', 'wlan0']
+    #performance = [10,1]
     Uid = Unique_id(network)
     #first Discover server MAC Address
     procs = []
@@ -106,7 +99,7 @@ def main():
                 #    img = cv2.imread(img+'.jpg',cv2.IMREAD_COLOR)
                 #except:
                 #    img = np.full((100,100,3),(255,0,255),dtype=np.uint8)
-                img = np.full((100,100,3),(255,0,255),dtype=np.uint8)
+                img = np.full((1000,1000,3),(255,0,255),dtype=np.uint8)
                 proc = threading.Thread(target=send_img, args=(interface, network, performance, Uid, img ,))
             else:
                 proc = threading.Thread(target=send_msg, args=(interface, network, performance, Uid, msg ,))
@@ -117,7 +110,7 @@ def main():
             proc.join()
 
         if msg=='exit':
-            print("\nExit message")
+            print("\nExit Program")
             break
 
 
